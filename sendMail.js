bb28c63504ff026ff5feaefefed1d9c99b5b18e5
Utkp5 <utkp200@gmail.com>
2022-09-18 13:54:55 +0530

    first commit

---
+++ sendMail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+
+  var transporter =  nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    // port: 465,
+    service: "gmail",
+    auth: {
+      user: "utkptesting@gmail.com", // user email
+      pass: "zelhxeygvpkhpisp", // user password
+    },
+  });
+
+  // send mail with defined transport object
+  var mailOptions = {
+    from: "utkptesting@gmail.com", // sender address
+    to: "rahat@gmail.com", // list of receivers 
+    subject: "Thanks for using our Website", // Subject line
+    text: "Do not reply on this email as this email is bot", // plain text body
+    // html: "<h1>Do not reply on this email as this email is bot</h1>", // html body here we can use html also
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent Sucessfully");
+    }
+  });
+
+  
+  
